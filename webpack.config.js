77f1d62661de06194a76f49eaa78cc189a2717ae
bruno.wozniak <bruno.wozniak@CORP.AMAZONWORKSPACES.COM>
2017-10-25 17:34:35 +0200

    Changed /public to public in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, '/public'),
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
@@ -23,6 +23,6 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, '/public')
+        contentBase: path.join(__dirname, 'public')
     }
 };
